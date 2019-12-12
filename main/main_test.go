81497299d76929af681adba00048a34dd379c79d
Marcin Maliszkiewicz <marcin.maliszkiewicz@blockfactory.com>
2019-12-12 14:26:42 +0100

    Add advanced workflow execution test

--- main/main_test.go
+++ main/main_test.go
@@ -2,8 +2,14 @@
 
 package main
 
-import "testing"
+import (
+	"testing"
+
+	cfg "github.com/ProxeusApp/proxeus-core/main/config"
+)
 
 func TestCoverage(m *testing.T) {
+	cfg.Config.Settings.TestMode = "true"
+	cfg.Config.ServiceAddress = ":1323"
 	main()
 }
