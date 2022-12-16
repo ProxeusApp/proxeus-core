c82051f5167454c44dbe8de2d3964f69007c7d1b
Afonina Tatiana <99973342+tafonina@users.noreply.github.com>
2022-12-16 18:27:37 +0200

    test negative scenario

--- ui/wallet/src/ProxeusEthereum/config/service-config.js
+++ ui/wallet/src/ProxeusEthereum/config/service-config.js
@@ -1,22 +1,4 @@
 const serviceConfig = {
-  goerli: {
-    DEFAULT_GAS_REGULAR: 1000000,
-    DEFAULT_GAS_CREATE: 4000000,
-    XES_TOKEN_ADDRESS: '0x15FeA089CC48B4f4596242c138156e3B53579B37',
-    PROXEUS_FS_ADDRESS: '0x66FF4FBF80D4a3C85a54974446309a2858221689',
-    // When the PROXEUS_FS_ADDRESS changes add the old address to the list below so that file validation check also past contracts
-    PROXEUS_FS_PAST_ADDRESSES: [
-    ]
-  },
-  'polygon-mumbai': {
-    DEFAULT_GAS_REGULAR: 1000000,
-    DEFAULT_GAS_CREATE: 4000000,
-    XES_TOKEN_ADDRESS: '0xf94BdC648A30719fF0cf91B436f9819F7804e1a0',
-    PROXEUS_FS_ADDRESS: '0x00119d8C02bbC4c1231D054BB2813792B4411Ed5',
-    // When the PROXEUS_FS_ADDRESS changes add the old address to the list below so that file validation check also past contracts
-    PROXEUS_FS_PAST_ADDRESSES: [
-    ]
-  },
   main: {
     DEFAULT_GAS_REGULAR: 1000000,
     DEFAULT_GAS_CREATE: 4000000,
