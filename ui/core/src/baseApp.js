8565c1a256f6140653ea39d50fd32becc350bdda
Tatiana <tafonina@s-pro.io>
2022-12-19 12:10:16 +0200

    remove console metamask error

--- ui/core/src/baseApp.js
+++ ui/core/src/baseApp.js
@@ -409,14 +409,12 @@ export default {
     this.loadMe()
 
     // when accounts loaded register accountsChanged handler and reload page if user changes the account
-    web3.eth.getAccounts(() => {
-      window.ethereum.on('accountsChanged', function () {
-        window.location.reload()
-      })
-      window.ethereum.on('chainChanged', function (e) {
-        console.log('Chain changed')
-        window.location.reload()
-      })
+    window.ethereum.on('accountsChanged', function () {
+      window.location.reload()
+    })
+    window.ethereum.on('chainChanged', function (e) {
+      console.log('Chain changed')
+      window.location.reload()
     })
   }
 }
