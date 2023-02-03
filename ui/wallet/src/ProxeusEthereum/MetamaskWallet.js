a890af12a6bf7b78e8baac33942f081b4567dfab
Tatiana <tafonina@s-pro.io>
2023-02-03 10:58:47 +0200

    add console logs on default accounts setup and on sign request

--- ui/wallet/src/ProxeusEthereum/MetamaskWallet.js
+++ ui/wallet/src/ProxeusEthereum/MetamaskWallet.js
@@ -5,11 +5,13 @@ class MetamaskWallet {
   }
 
   async setupDefaultAccount () {
+    console.log('Setting up default account')
     this.web3.eth.defaultAccount = (await this.web3.eth.requestAccounts())[0]
   }
 
   async signMessage (message) {
     const address = this.getCurrentAddress()
+    console.log('Sign message request, address: ', address)
     return this.web3.eth.personal.sign(message, address)
   }
 
@@ -22,7 +24,7 @@ class MetamaskWallet {
   getCurrentAddress () {
     this.web3.eth.getAccounts().then((address) => {
       if (this.web3.eth.defaultAccount !== address[0]) {
-        console.log('user changed the account')
+        console.log('User changed the account')
       }
     })
     return this.web3.eth.defaultAccount
