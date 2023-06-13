07edf757c05922a86f63aa5d1ab6d9372455142f
Oleg Lavrovsky <loleg@users.noreply.github.com>
2023-06-13 10:47:29 +0200

    Network usage notes

--- ui/wallet/src/ProxeusEthereum/config/service-config.js
+++ ui/wallet/src/ProxeusEthereum/config/service-config.js
@@ -17,15 +17,6 @@ const serviceConfig = {
     PROXEUS_FS_PAST_ADDRESSES: [
     ]
   },
-  'polygon-mumbai': {
-    DEFAULT_GAS_REGULAR: 1000000,
-    DEFAULT_GAS_CREATE: 4000000,
-    XES_TOKEN_ADDRESS: '0xf94BdC648A30719fF0cf91B436f9819F7804e1a0',
-    PROXEUS_FS_ADDRESS: '0x00119d8C02bbC4c1231D054BB2813792B4411Ed5',
-    // When the PROXEUS_FS_ADDRESS changes add the old address to the list below so that file validation check also past contracts
-    PROXEUS_FS_PAST_ADDRESSES: [
-    ]
-  },
   main: {
     DEFAULT_GAS_REGULAR: 1000000,
     DEFAULT_GAS_CREATE: 4000000,
@@ -44,11 +35,11 @@ const serviceConfig = {
     PROXEUS_FS_PAST_ADDRESSES: [
     ]
   },
-  ropsten: {
+  'polygon-mumbai': {
     DEFAULT_GAS_REGULAR: 1000000,
     DEFAULT_GAS_CREATE: 4000000,
-    XES_TOKEN_ADDRESS: '0x84E0b37e8f5B4B86d5d299b0B0e33686405A3919',
-    PROXEUS_FS_ADDRESS: '0x1d3e5c81bf4bc60d41a8fbbb3d1bae6f03a75f71', // Eternal Storage at: 0x7b83acb323fd4bbd874c1e9c295e0f486d94b233
+    XES_TOKEN_ADDRESS: '0xf94BdC648A30719fF0cf91B436f9819F7804e1a0',
+    PROXEUS_FS_ADDRESS: '0x00119d8C02bbC4c1231D054BB2813792B4411Ed5',
     // When the PROXEUS_FS_ADDRESS changes add the old address to the list below so that file validation check also past contracts
     PROXEUS_FS_PAST_ADDRESSES: [
     ]
