f9dac475093956f722c9ff14795a2781b6a3b381
slavas490 <slavas490@gmail.com>
2024-01-16 19:50:48 +0200

    WIP: initial commit

---
+++ ui/core/public/static/dynamic-config/index.js
@@ -0,0 +1,66 @@
+export default {
+  "apply": true,
+  "company": {
+    "logo": {
+      "lightTheme": {
+        "path": "/static/dynamic-config/img/company_logo_light.svg"
+      },
+      "darkTheme": {
+        "path": "/static/dynamic-config/img/company_logo_dark.svg"
+      },
+      "style": {
+        "home": {
+          "height": "41px"
+        },
+        "adminDashboard": {
+          "full": {
+            "width": "135px",
+            "height": "42px"
+          },
+          "small": {
+            "width": "39px",
+            "height": "42px"
+          }
+        },
+        "userDashboard": {
+          "full": {
+            "width": "195px",
+            "height": "60px",
+            "margin": "1rem auto 0.5rem auto"
+          },
+          "small": {
+            "width": "39px",
+            "height": "42px",
+            "margin": "1rem auto 0.5rem -0.5rem"
+          }
+        }
+      }
+    },
+    "copyrightText": {
+      "lang": {
+        "en": "Developed by",
+        "de": "Entwickelt von"
+      },
+      "logo": {
+        "style": {
+          "height": "20px"
+        }
+      },
+      "link": "https://s-pro.io"
+    }
+  },
+  "homePage": {
+    "platformTitle": {
+      "lang": {
+        "en": "Proxeus Demo Server",
+        "de": "Proxeus Demo"
+      }
+    },
+    "platformDescription": {
+      "lang": {
+        "en": "Welcome to the Proxeus demo server, sponsored by S-PRO. This service is running on the Ethereum Sepolia testnet. Please note that all content added here gets reset once per day. For more information, click the “Handbook” link below, or visit <a href=\"https://proxeus.org/\" target=\"_blank\">proxeus.org</a>",
+        "de": "Willkommen auf dem Proxeus-Demoserver, gesponsert von S-PRO. Dieser Dienst läuft im Ethereum Sepolia-Testnetz. Bitte beachten Sie, dass alle hier hinzugefügten Inhalte einmal täglich zurückgesetzt werden. Für weitere Informationen klicken Sie unten auf den Link \"Handbuch\" oder besuchen Sie <a href=\"https://proxeus.org/\" target=\"_blank\">proxeus.org</a>"
+      }
+    }
+  }
+}
