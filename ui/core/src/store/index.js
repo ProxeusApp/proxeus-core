31da6ab2e366e035e51e34fae9b8845ac3f13a27
Oleg Lavrovsky <loleg@users.noreply.github.com>
2025-07-07 13:34:32 +0200

    Webpack5

--- ui/core/src/store/index.js
+++ ui/core/src/store/index.js
@@ -1,16 +1,16 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import forms from './forms'
-import signatures from './signatures'
+import Vue from "vue";
+import Vuex from "vuex";
+import forms from "./forms";
+import signatures from "./signatures";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
-const debug = process.env.NODE_ENV !== 'production'
+const debug = process.env.NODE_ENV !== "production";
 
 export default new Vuex.Store({
   modules: {
     forms,
-    signatures
+    signatures,
   },
-  strict: debug
-})
+  strict: debug,
+});
